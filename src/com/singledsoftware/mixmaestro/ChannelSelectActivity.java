f33c9814d630224d9ce52bbbf4b07cb4544e0c13
Judson D Neer <judson.neer@gmail.com>
2013-12-07 17:00:53 -0800

    Add header information to all Java and XML files. Closes #3.

--- src/com/singledsoftware/mixmaestro/ChannelSelectActivity.java
+++ src/com/singledsoftware/mixmaestro/ChannelSelectActivity.java
@@ -1,3 +1,5 @@
+// Copyright 2013 Judson D Neer
+
 package com.singledsoftware.mixmaestro;
 
 import android.os.Bundle;
@@ -5,17 +7,31 @@ import android.app.Activity;
 import android.view.Menu;
 import android.widget.ListView;
 
+/**
+ * Provides channel selection for the Mix Maestro client application.
+ *
+ * @see ScoresheetActivity
+ * @author Judson D Neer
+ */
 public class ChannelSelectActivity extends Activity {
 
+    // Stores a reference to the channel list widget.
     private ListView channelList;
 
+    /**
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_channel_select);
+        // Grab a reference to a view widget.
         channelList = (ListView)this.findViewById(R.id.channel_list);
     }
 
+    /**
+     * @see android.app.Activity#onCreateOptionsMenu(Menu menu)
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
